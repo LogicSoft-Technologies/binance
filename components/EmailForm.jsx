5b078b0a0fb61630f76b2ce22ecae7dfb9553b29
Elijah Alexander <elijahkylealexander@gmail.com>
2026-03-11 21:18:04 +0100

    first commit

--- components/EmailForm.jsx
+++ components/EmailForm.jsx
@@ -286,7 +286,7 @@ export default function EmailForm() {
           textAlign: "center", marginTop: "20px",
           fontSize: "12px", color: B.textMuted, lineHeight: "18px",
         }}>
-          Emails are delivered instantly · Replies go to your support inbox
+          Emails are delivered instantly· Replies go to your support inbox
         </p>
 
       </div>
